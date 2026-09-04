7b54701fdc3fdd0d6eb2f307fd09455794c3780a
jeeteshsurana <jeeteshsurana@gmail.com>
2026-09-04 14:13:19 +0530

    Created 26 modules for mini-projects, UI, and Network

---
+++ homealert/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.pqc.android_bites.homealert"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+}
+

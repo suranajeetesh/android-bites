923fb9e345e48e98ee6b1bc0820aaeb3d851c7c3
jeeteshsurana <jeeteshsurana@gmail.com>
2026-09-04 15:43:54 +0530

    Increased minSdkVersion to 24 in app module to fix manifest merger error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.pqc.android_bites"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
